a75c3a9c75f5f6015c2bcad9883313ff91f5d5b5
Carlos Bruno <cbcarlos08@gmail.com>
2018-06-15 10:49:24 -0400

    Aula 23 Verificando Token

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,6 @@
 
 const restify = require('restify')
-
+const jwt = require('jsonwebtoken')
 const server = restify.createServer()
 
 const routes = require('../http/routes')
@@ -9,11 +9,19 @@ const cors = require('./cors')
 server.pre(cors.preflight)
 server.use(cors.actual)
 server.use(restify.plugins.bodyParser())
-server.use((req, res, next) => {
-  if (!req.header['x-access-token']) {
+server.use(async (req, res, next) => {
+  const token = req.headers['x-access-token']
+  if (!token) {
     res.send(403, {error: 'Token não fornecido'})
     return false
   }
+  await jwt.verify(token, process.env.JWT_SECRET, (error, decoded) => {
+    if (error) {
+      res.send(403, { error: 'Falha ao autenticar o token' })
+    } else {
+      req.decoded = decoded
+    }
+  })
   next()
 })
 
