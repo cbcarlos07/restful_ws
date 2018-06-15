69bed9ad1b6e7ee2ab307cb507608be208fb456d
Carlos Bruno <cbcarlos08@gmail.com>
2018-06-15 07:26:41 -0400

    Aula 18

--- src/services/mysql/categories.js
+++ src/services/mysql/categories.js
@@ -41,11 +41,11 @@ const categories = deps => {
       return new Promise((resolve, reject) => {
         const { connection, errorHandler } = deps
         connection.query('DELETE FROM categories WHERE id = ?', [id], (error, results) => {
-          if (error) {
+          if (error || !results.affectedRows) {
             errorHandler(error, `Falha ao remover a categoria de id ${id}`, reject)
             return false
           }
-          resolve({message: 'Categoria removida com sucesso'})
+          resolve({message: 'Categoria removida com sucesso', affectedRows: results.affectedRows})
         })
       })
     }
